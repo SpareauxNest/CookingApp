6caa395024a93b38cecfac7e9b01529204f3b5e3
Spareaux <spareauxnest@gmail.com>
2023-10-13 15:00:00 -0400

    Created first test :D

---
+++ src/test/java/me/RecipeBookTest.java
@@ -0,0 +1,18 @@
+package me;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeBookTest {
+
+    @BeforeAll
+    public void setUp(){
+
+    }
+    @Test
+    public void addTest(){
+
+    }
+}
