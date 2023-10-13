996580fb40703b6324340abd414423bab9345b4e
Darian Evans <darian.janelle.evans@gmail.com>
2023-10-13 16:04:56 -0400

    added recipe test and test dependencies

--- src/main/java/me/model/Recipe.java
+++ src/main/java/me/model/Recipe.java
@@ -1,5 +1,6 @@
 package me.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Recipe {
@@ -8,4 +9,15 @@ public class Recipe {
     private List<Ingredient> ingredients;
 
     private String link;
+
+    public Recipe(String name, String link, List<Ingredient> ingredients) {
+        this.name = name;
+        this.link = link;
+        this.ingredients = ingredients;
+    }
+
+    public Ingredient addIngredient(String name, int quantity, String unit) {
+        return new Ingredient(name, quantity, unit);
+
+    }
 }
