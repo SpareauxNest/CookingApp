7834cc40394e0dbd893ec2a2bc2ce955b385d355
Spareaux <spareauxnest@gmail.com>
2023-10-17 14:15:57 -0400

    NOTES: Test classes should be annotated with @TestInstance(TestInstance.Lifecycle.PER_CLASS) so they know they are test classes. The error of "ingredients.this is null" means that we needed to instantiate the variable. List<> is an interface, meaning we need to create the object rather than just referring to it. ArrayList is a class, meaning that it creates the object when we instantiate it.

--- src/main/java/me/model/Ingredient.java
+++ src/main/java/me/model/Ingredient.java
@@ -16,28 +16,4 @@ public class Ingredient {
     public static Ingredient build(String name, int quantity, String unit){
         return new Ingredient(name, quantity, unit);
     }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public String getUnit() {
-        return unit;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public void setUnit(String unit) {
-        this.unit = unit;
-    }
 }
--- src/test/java/me/RecipeBookTest.java
+++ src/test/java/me/RecipeBookTest.java
@@ -2,13 +2,12 @@ package me;
 
 import me.model.Ingredient;
 import me.model.Recipe;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RecipeBookTest {
     private Ingredient testIngredient1;
     private Ingredient testIngredient2;
@@ -17,9 +16,11 @@ class RecipeBookTest {
     private Recipe testRecipe2;
     private List<Recipe> testRecipes;
 
-    private RecipeBook recipeBook;
+    private RecipeBook recipeBook = new RecipeBook();
     @BeforeAll
     public void setUp(){
+        ingredients = new ArrayList<>();
+        testRecipes = new ArrayList<>();
         testIngredient1 = Ingredient.build("garlic", 3, "cloves");
         testIngredient2 = Ingredient.build("butter", 4, "Tablespoons");
         ingredients.add(testIngredient1);
@@ -30,7 +31,7 @@ class RecipeBookTest {
         testRecipes.add(testRecipe2);
     }
     @Test
-    public void createRecipeTest(){
+    public void createRecipesTest(){
         recipeBook.addRecipe(testRecipe1);
         List<Recipe> actual = recipeBook.addRecipe(testRecipe2);
         Assertions.assertEquals(actual, testRecipes);
